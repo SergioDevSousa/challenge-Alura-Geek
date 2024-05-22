0082584c0a7ae6f424a957309467e73dcd7ff411
SergioDevSousa <166451865+SergioDevSousa@users.noreply.github.com>
2024-05-22 18:02:02 -0300

    atualiza alurageek

---
+++ js/servicos/produtos-serv.js
@@ -0,0 +1,8 @@
+const productList () => {
+    return fetch ("http://localhost:5501/products")
+    .then((res) => res.json())
+    .catch((err) => console(err));
+}
+export const serviceProduts = {
+    productList,
+};
